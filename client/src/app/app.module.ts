0c41b17aceb88355026ea1548163c53d5c492032
isakgranqvist2021 <isakwebdev@gmail.com>
2022-03-19 13:37:25 +0100

    organize imports

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,73 +1,74 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { DragDropModule } from '@angular/cdk/drag-drop';
 import { FormsModule } from '@angular/forms';
-import { AppRoutingModule } from './app-routing.module';
-import { MatTooltipModule } from '@angular/material/tooltip';
-import { MatRippleModule } from '@angular/material/core';
-import { MatTabsModule } from '@angular/material/tabs';
 import { NgxEchartsModule } from 'ngx-echarts';
-import { DndDirective } from './components/file-upload/dnd.directive';
+import { MatTabsModule } from '@angular/material/tabs';
+import { HttpClientModule } from '@angular/common/http';
+import { DragDropModule } from '@angular/cdk/drag-drop';
+import { MatRippleModule } from '@angular/material/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { MatTooltipModule } from '@angular/material/tooltip';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { AppComponent } from './app.component';
+
+import { DndDirective } from './components/file-upload/dnd.directive';
+import { ErrorComponent } from './components/alerts/alerts.component';
+import { ToolbarComponent } from './components/toolbar/toolbar.component';
+import { NavigationComponent } from './components/navigation/navigation.component';
+import { FileUploadComponent } from './components/file-upload/file-upload.component';
+import { EditUserFormComponent } from './components/edit-user-form/edit-user-form.component';
+
 import { HomeComponent } from './pages/home/home.component';
 import { LoginComponent } from './pages/login/login.component';
-import { RegisterComponent } from './pages/register/register.component';
-import { NavigationComponent } from './components/navigation/navigation.component';
-import { HttpClientModule } from '@angular/common/http';
-import { ErrorComponent } from './components/alerts/alerts.component';
-import { ServerManagementComponent } from './pages/server-management/server-management.component';
-import { PublishComponent } from './pages/publish/publish.component';
-import { SettingsComponent } from './pages/settings/settings.component';
-import { ImageManagementComponent } from './pages/image-management/image-management.component';
-import { BrowseComponent } from './pages/browse/browse.component';
-import { EditPostComponent } from './pages/browse/edit/edit.component';
-import { FileUploadComponent } from './components/file-upload/file-upload.component';
-import { DashboardComponent } from './pages/dashboard/dashboard.component';
 import { UsersComponent } from './pages/users/users.component';
-import { EditUserFormComponent } from './components/edit-user-form/edit-user-form.component';
+import { BrowseComponent } from './pages/browse/browse.component';
+import { PublishComponent } from './pages/publish/publish.component';
 import { EditUserComponent } from './pages/users/edit/edit.component';
-import { ToolbarComponent } from './components/toolbar/toolbar.component';
+import { EditPostComponent } from './pages/browse/edit/edit.component';
+import { RegisterComponent } from './pages/register/register.component';
+import { SettingsComponent } from './pages/settings/settings.component';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { ImageManagementComponent } from './pages/image-management/image-management.component';
+import { ServerManagementComponent } from './pages/server-management/server-management.component';
+
+import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    HomeComponent,
-    LoginComponent,
-    RegisterComponent,
-    NavigationComponent,
-    ErrorComponent,
-    ServerManagementComponent,
-    PublishComponent,
-    SettingsComponent,
-    ImageManagementComponent,
-    BrowseComponent,
-    DndDirective,
-    EditPostComponent,
-    FileUploadComponent,
-    DashboardComponent,
-    UsersComponent,
-    EditUserFormComponent,
-    EditUserComponent,
-    ToolbarComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    FormsModule,
-    HttpClientModule,
-    DragDropModule,
-    BrowserAnimationsModule,
-    MatTooltipModule,
-    MatRippleModule,
-    MatTabsModule,
-    NgxEchartsModule.forRoot({
-      echarts: () => import('echarts')
-    })
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+	declarations: [
+		AppComponent,
+		HomeComponent,
+		LoginComponent,
+		RegisterComponent,
+		NavigationComponent,
+		ErrorComponent,
+		ServerManagementComponent,
+		PublishComponent,
+		SettingsComponent,
+		ImageManagementComponent,
+		BrowseComponent,
+		DndDirective,
+		EditPostComponent,
+		FileUploadComponent,
+		DashboardComponent,
+		UsersComponent,
+		EditUserFormComponent,
+		EditUserComponent,
+		ToolbarComponent,
+	],
+	imports: [
+		BrowserModule,
+		AppRoutingModule,
+		FormsModule,
+		HttpClientModule,
+		DragDropModule,
+		BrowserAnimationsModule,
+		MatTooltipModule,
+		MatRippleModule,
+		MatTabsModule,
+		NgxEchartsModule.forRoot({
+			echarts: () => import('echarts'),
+		}),
+	],
+	providers: [],
+	bootstrap: [AppComponent],
 })
-
-export class AppModule { }
- 
+export class AppModule {}
--- client/src/app/components/edit-user-form/edit-user-form.component.ts
+++ client/src/app/components/edit-user-form/edit-user-form.component.ts
@@ -6,63 +6,78 @@ import { StateService } from 'src/app/services/state.service';
 import { UserInitial, NewDataInitial, ConfigInitial } from './utils';
 
 @Component({
-  selector: 'app-edit-user-form',
-  templateUrl: './edit-user-form.component.html',
-  styleUrls: ['./edit-user-form.component.scss']
+	selector: 'app-edit-user-form',
+	templateUrl: './edit-user-form.component.html',
+	styleUrls: ['./edit-user-form.component.scss'],
 })
 export class EditUserFormComponent implements OnInit {
-  @Output() onSuccess = new EventEmitter<string>(); // will be sent when the update method was successful
-  @Input() user: User = UserInitial(); // the user that is being modified
-  public newData: any = NewDataInitial(); // new data 
-  public config: any = ConfigInitial(); // any config 
-  public privileges: string = '0'; // data binding prop
-  private oldData?: User; // the initial value when the component is loaded
+	@Output() onSuccess = new EventEmitter<string>(); // will be sent when the update method was successful
+	@Input() user: User = UserInitial(); // the user that is being modified
+	public newData: any = NewDataInitial(); // new data
+	public config: any = ConfigInitial(); // any config
+	public privileges: string = '0'; // data binding prop
+	private oldData?: User; // the initial value when the component is loaded
 
-  constructor(private stateService: StateService, private userService: UserService) {}
+	constructor(
+		private stateService: StateService,
+		private userService: UserService
+	) {}
 
-  ngOnInit(): void {
-    this.oldData = this.user;
-    this.newData.email = this.user.email;
+	ngOnInit(): void {
+		this.oldData = this.user;
+		this.newData.email = this.user.email;
 
-     /* 
+		/* 
         set as string because the <select> converts
         the number to a string and then it can't be passed as an argument
         to the parseInt() function because the interface
         is of type number 
-     */ this.privileges = this.oldData.admin.toString();
-  }
+     */
 
-  private emitAlert(err: boolean, msg: string): void {
-    this.stateService.setErrorSubject({
-      show: true,
-      error: err,
-      text: msg
-    })
-  }
+		this.privileges = this.oldData.admin.toString();
+	}
 
-  public update(): void {
-    if(confirm('Confirm Account Update')) {
-      this.userService.update({
-        oldData: this.oldData,
-        newData: this.newData,
-        config: this.config
-      }).subscribe((response: HTTPResponse) => response.success
-      ? (() => {
-        this.emitAlert(false, response.message);
-        this.onSuccess.emit('Your account has been updated, please sign back in.')
-      })() : this.emitAlert(true, response.message));
-    }
-  } 
+	private emitAlert(err: boolean, msg: string): void {
+		this.stateService.setErrorSubject({
+			show: true,
+			error: err,
+			text: msg,
+		});
+	}
 
-  public updatePrivileges(): void {
-    if(confirm('Confirm Privileges Update')) {
-      this.userService.setAdmin({
-        admin: parseInt(this.privileges),
-        email: this.oldData?.email
-      }).subscribe((response: HTTPResponse) => 
-      response.success 
-      ? this.emitAlert(false, response.message)
-      : this.emitAlert(true, response.message));
-    }
-  }
+	public update(): void {
+		if (confirm('Confirm Account Update')) {
+			this.userService
+				.update({
+					oldData: this.oldData,
+					newData: this.newData,
+					config: this.config,
+				})
+				.subscribe((response: HTTPResponse) =>
+					response.success
+						? (() => {
+								this.emitAlert(false, response.message);
+								this.onSuccess.emit(
+									'Your account has been updated, please sign back in.'
+								);
+						  })()
+						: this.emitAlert(true, response.message)
+				);
+		}
+	}
+
+	public updatePrivileges(): void {
+		if (confirm('Confirm Privileges Update')) {
+			this.userService
+				.setAdmin({
+					admin: parseInt(this.privileges),
+					email: this.oldData?.email,
+				})
+				.subscribe((response: HTTPResponse) =>
+					response.success
+						? this.emitAlert(false, response.message)
+						: this.emitAlert(true, response.message)
+				);
+		}
+	}
 }
